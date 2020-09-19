e218d59b2eb64d5257d97433301c053061defafb
MiaTrinity <mia.trinity.codes@gmail.com>
2020-09-19 11:07:55 +0200

    before nomad sessio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+//custom types start with c -> e.g. boxShadow cg = custom grey
 module.exports = {
   purge: [],
   theme: {
@@ -5,6 +6,7 @@ module.exports = {
       xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
       cg: '0 0 0 1px rgba(37,37,37,.12)',
       cp: '0 0 0 .2rem rgba(184,50,128, 0.8)',
+      nav: '0 0.5px 0.5px rgba(0,0,0,0.4)',
       sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
       md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
@@ -16,7 +18,27 @@ module.exports = {
       focus: '0 0 0 3px rgba(66, 153, 225, 0.5)',
       none: 'none',
     },
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1150px',
+    },
+    extend: {
+      // gridTemplateColumns: {
+      //   // 3:1 two column layout
+      //   '3-1': '3fr 1fr',
+      // },
+      transitionTimingFunction: {
+        ease: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
+      },
+      // borderRadius: { 
+      //   circle: '50%',
+      // },
+      maxWidth: {
+        xxs: '180px',
+      },
+    },
   },
   variants: {
     boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
